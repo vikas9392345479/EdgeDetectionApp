83760c2cec823229b15831d4b98a97617b7ac64f
Eera Vikas <eeravikas1307@gmail.com>
2025-10-08 01:33:37 +0530

    Add TextureView and reference it in MainActivity for camera preview

--- app/src/main/java/com/example/edgedetectionapp/MainActivity.kt
+++ app/src/main/java/com/example/edgedetectionapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.edgedetectionapp
 
 import android.os.Bundle
+import android.view.TextureView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textureView: TextureView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textureView = findViewById(R.id.textureView)
     }
 }
